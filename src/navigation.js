8e59601cf85de764f77fd7da2b7e307a7968600b
Ralph Largo <devlargs@gmail.com>
2018-07-11 17:09:42 +0800

    removed App.js

---
+++ src/navigation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'native-base';
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Navigation</Text>
+            </View>
+        )
+    }
+}
